a99c175bde0f003674629b719f90f6d25bb2c3e2
TatsunoriSPU <tatsunori.kat@spumail.net>
2025-03-20 19:37:02 +0700

    edit-filename

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login/login";
 import PurchaseForm from "./PurchaseForm/purchaseForm"; 
-import ProcurementSystem from "./ProcurementSystem/procurementSystem"; 
+import ProcurementSystem from "./ProcurementSystem/procurement"; 
 import CardList from "./CardList/cardList";
 
 const App = () => {
--- frontend/src/ProcurementSystem/procurement.jsx
+++ frontend/src/ProcurementSystem/procurement.jsx
@@ -1,5 +1,5 @@
 
-import "./ProcurementSystem.css";
+import "./procurement.css";
 
 const ProcurementSystem = () => {
   const data = [
